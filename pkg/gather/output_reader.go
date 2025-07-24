7d99c9bf63e550e55f683a86e4a9e9f548a04628
Nir Soffer <nsoffer@redhat.com>
2025-07-24 19:13:11 +0300

    gather: Introduce OutputReader
    OutputReader makes it easy to list and read gathered resources from the
    output directory.
    
    Like OutputDirectory is works on a single gather directory. To read read
    multiple clusters data create multiple readers. It may more useful to
    support multiple directories, but inconsistent with current gather API.
    
    The reader provide the following functions:
    
    - ListResources(namespace, resource) - returns list of resource names.
      This is required when the resource name contains random part.
    - ReadResource(namespace, resource, name) - read resource data
    
    Empty namespace access cluster scope resources.
    
    I considered adding OpenResource() but this add lot of boilerplate to
    read the data and closing the file, and resources are small so it is not
    helpful.

---
+++ pkg/gather/output_reader.go
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: The kubectl-gather authors
+// SPDX-License-Identifier: Apache-2.0
+
+package gather
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type OutputReader struct {
+	base string
+}
+
+// NewOutputReader creates a new OutputReader instance.
+func NewOutputReader(path string) *OutputReader {
+	return &OutputReader{base: path}
+}
+
+// ListResoruces lists resource names in namespace.
+func (r *OutputReader) ListResources(namespace, resource string) ([]string, error) {
+	var resourceDir string
+	if namespace == "" {
+		resourceDir = filepath.Join(r.base, clusterDir, resource)
+	} else {
+		resourceDir = filepath.Join(r.base, namespacesDir, namespace, resource)
+	}
+
+	entries, err := os.ReadDir(resourceDir)
+	if err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, e := range entries {
+		// Skip pod directory.
+		if e.IsDir() {
+			continue
+		}
+		resourceName := strings.TrimSuffix(e.Name(), resourceSuffix)
+		names = append(names, resourceName)
+	}
+
+	return names, nil
+}
+
+// ReadResource reads named resource data.
+func (r *OutputReader) ReadResource(namespace, resource, name string) ([]byte, error) {
+	var resourcePath string
+	if namespace == "" {
+		resourcePath = filepath.Join(r.base, clusterDir, resource, name+resourceSuffix)
+	} else {
+		resourcePath = filepath.Join(r.base, namespacesDir, namespace, resource, name+resourceSuffix)
+	}
+	return os.ReadFile(resourcePath)
+}
