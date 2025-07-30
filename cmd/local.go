84797567b7089316ea24a31ec4440e20b2ad94bd
Parikshith <parikshithb@gmail.com>
2025-07-30 18:30:27 +0530

    cmd: rename cluster variables to avoid naming conflict
    Rename cluster loop variables to clusterConfig to prevent conflict
    with the new global cluster flag variable.
    
    Signed-off-by: Parikshith <parikshithb@gmail.com>

--- cmd/local.go
+++ cmd/local.go
@@ -16,37 +16,37 @@ type result struct {
 	Err   error
 }
 
-func localGather(clusters []*clusterConfig) {
+func localGather(clusterConfigs []*clusterConfig) {
 	start := time.Now()
 
 	wg := sync.WaitGroup{}
-	results := make(chan result, len(clusters))
+	results := make(chan result, len(clusterConfigs))
 
-	for i := range clusters {
-		cluster := clusters[i]
+	for i := range clusterConfigs {
+		clusterConfig := clusterConfigs[i]
 
-		if cluster.Context != "" {
-			log.Infof("Gathering from cluster %q", cluster.Context)
+		if clusterConfig.Context != "" {
+			log.Infof("Gathering from cluster %q", clusterConfig.Context)
 		} else {
 			log.Info("Gathering on cluster")
 		}
 		start := time.Now()
 
-		directory := filepath.Join(directory, cluster.Context)
+		directory := filepath.Join(directory, clusterConfig.Context)
 
 		options := gather.Options{
 			Kubeconfig: kubeconfig,
-			Context:    cluster.Context,
+			Context:    clusterConfig.Context,
 			Namespaces: namespaces,
 			Addons:     addons,
-			Log:        log.Named(cluster.Context),
+			Log:        log.Named(clusterConfig.Context),
 		}
 
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
-			g, err := gather.New(cluster.Config, directory, options)
+			g, err := gather.New(clusterConfig.Config, directory, options)
 			if err != nil {
 				results <- result{Err: err}
 				return
@@ -59,9 +59,9 @@ func localGather(clusters []*clusterConfig) {
 			}
 
 			elapsed := time.Since(start).Seconds()
-			if cluster.Context != "" {
+			if clusterConfig.Context != "" {
 				log.Infof("Gathered %d resources from cluster %q in %.3f seconds",
-					g.Count(), cluster.Context, elapsed)
+					g.Count(), clusterConfig.Context, elapsed)
 			} else {
 				log.Infof("Gathered %d resources on cluster in %.3f seconds",
 					g.Count(), elapsed)
@@ -87,5 +87,5 @@ func localGather(clusters []*clusterConfig) {
 	}
 
 	log.Infof("Gathered %d resources from %d clusters in %.3f seconds",
-		count, len(clusters), time.Since(start).Seconds())
+		count, len(clusterConfigs), time.Since(start).Seconds())
 }
